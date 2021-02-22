2d23840c2eea0ff0f2fb354785ec25913202b129
Ehsanul Mostofa <18-38383-2@student.aiub.edu>
2021-02-22 13:58:02 +0600

    done

--- FormValidFinal.php
+++ FormValidFinal.php
@@ -1,4 +1,6 @@
 <?php
+    $name="";
+	$err_name="";
 	$uname="";
 	$err_uname="";
 	$pass="";
@@ -16,6 +18,10 @@
 	 ini_set('log_errors', '0'); ini_set('error_log', './');
 	 if(isset($_POST["submit"]))
 	 {
+		 if(empty($_POST["name"]))
+		 {
+			 $err_name="[ NAME REQUIRED ]";
+		 }
 		 if(empty($_POST["uname"]))
 		 {
 			 $err_uname="[ USERNAME REQUIRED ]";
@@ -105,29 +111,41 @@
 <html>
 	<head></head>
 	<body>
-		<h1>User Registration</h1>
 		<form action="" method="post">
+		<fieldset>
+          <legend><h1>User Registration</h1></legend>
 			<table>
+			    <tr>
+					<td><span><b>Name</b></span></td>
+					<td>:  <input type="text" name="name" value="<?php echo $name;?>">
+					<span><?php echo $err_name;?></span></td>
+
+				</tr>
 				<tr>
 					<td><span><b>Username</b></span></td>
-					<td>:<input type="text" name="uname" value="<?php echo $uname;?>">
+					<td>:  <input type="text" name="uname" value="<?php echo $uname;?>">
 					<span><?php echo $err_uname;?></span></td>
 
 				</tr>
 				<tr>
 					<td><span><b>Password</b></span></td>
-					<td>:<input type="password" name="pass" value="<?php echo $pass;?>">
+					<td>:  <input type="password" name="pass" value="<?php echo $pass;?>">
+					<span><?php echo $err_pass;?></span></td>
+				</tr>
+				<tr>
+					<td><span><b>Confirm Password</b></span></td>
+					<td>:  <input type="password" name="pass" value="<?php echo $pass;?>">
 					<span><?php echo $err_pass;?></span></td>
 				</tr>
 				<tr>
 					<td><span><b>Gender</b></span></td>
-					<td>:<input type="radio" name="gender" value="Male"><span>Male</span>
+					<td>:  <input type="radio" name="gender" value="Male"><span>Male</span>
 					    <input type="radio" name="gender" value="Female"><span>Female</span>
 						<span><?php echo "&nbsp ".$err_gender;?></span></td>
 				</tr>
 				<tr>
 					<td><span><b>Hobbies</b></span></td>
-					<td>:<input type="checkbox" name="Hobbies[]" value="Movies"><span>Movies</span>
+					<td>:  <input type="checkbox" name="Hobbies[]" value="Movies"><span>Movies</span>
 					    <input type="checkbox" name="Hobbies[]" value="Music"><span>Music</span>
 						<input type="checkbox" name="Hobbies[]" value="Games"><span>Games</span>
 						<input type="checkbox" name="Hobbies[]" value="Sports"><span>Sports</span>
@@ -148,24 +166,25 @@
 				</tr>
 				<tr>
 	 				<td><span><b>Bio</b></span></td>
-					 <td>:<textarea name="bio" value="<?php echo $bio;?>"></textarea>
+					 <td>:  <textarea name="bio" value="<?php echo $bio;?>"></textarea>
 					 <span><?php echo "&nbsp".$err_bio;?></span></td>
 				</tr>
 				<tr>
 					<td><span><b>Email</b></span></td>
-					<td>:<input type="email" name="email"></td>
+					<td>:  <input type="email" name="email"></td>
 				</tr>
 				<tr>
 					<td><span><b>Mobile/Phone</b></span></td>
-					<td>:<input type="tel" name="phone_no"></td>
+					<td>:  <input type="tel" name="phone_no"></td>
 				</tr>
 				<tr>
 					<td><span><b>Date</b></span></td>
-					<td>:<input type="date" name="date"></td>
+					<td>:  <input type="date" name="date"></td>
 				</tr>
 			</table>
 			<br>
-			<input type="submit" name="submit" value="submit">
+			<input type="Submit" name="Registration" value="submit">
+		  <fieldset>
 		</form>
 	</body>
 </html>
